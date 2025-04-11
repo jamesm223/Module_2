81b873b7339c8cb8510721a73b89a3d1d8bc5f1f
James Morton <jamesmorton@Jamess-MBP.localdomain>
2025-04-11 10:17:30 +0100

    added all of the extra functionalities using media queries and basic css/html

--- JS/script.js
+++ JS/script.js
@@ -2,3 +2,12 @@ function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
 }
+
+function myFunction1() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
